8333a38562d43de246170e0299f5fa245ed3d31f
Be-HinD <kr100475@gmail.com>
2024-09-04 15:48:07 +0900

    initial commit

---
+++ src/main/java/com/ssafy/roscamback/RoscamBackApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.roscamback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoscamBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoscamBackApplication.class, args);
+    }
+
+}
