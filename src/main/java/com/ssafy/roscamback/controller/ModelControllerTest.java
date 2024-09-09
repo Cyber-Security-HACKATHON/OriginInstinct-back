abd5ccc9f38cc2670274ab0807c4e12934f523d5
김인호 <linos98@naver.com>
2024-09-09 23:41:16 +0900

    [feat] 모델통신 부분개발 진행 중

--- src/main/java/com/ssafy/roscamback/controller/ModelControllerTest.java
+++ src/main/java/com/ssafy/roscamback/controller/ModelControllerTest.java
@@ -1,4 +1,8 @@
 package com.ssafy.roscamback.controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -7,21 +11,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/chat")
 public class ModelControllerTest {
 
-    private final RestTemplate restTemplate = new RestTemplate();
-    private final String url = "http://localhost:8000/predict";
-
-    public String getExplanation(String message) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", "application/json");
-
-        String requestJson = "{\"message\":\"" + message + "\"}";
-        HttpEntity<String> entity = new HttpEntity<>(requestJson, headers);
-
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
-
-
-        return response.getBody();
-    }
+//
+//    @PostMapping("/predict")
+//    public String getExplanation(String message) {
+////        HttpHeaders headers = new HttpHeaders();
+////        headers.set("Content-Type", "application/json");
+////
+////        String requestJson = "{\"message\":\"" + message + "\"}";
+////        HttpEntity<String> entity = new HttpEntity<>(requestJson, headers);
+////
+////        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+//
+//        return response.getBody();
+//    }
 }
--- src/main/java/com/ssafy/roscamback/response/ChatAnalysisResponse.java
+++ src/main/java/com/ssafy/roscamback/response/ChatAnalysisResponse.java
@@ -1,5 +1,12 @@
 package com.ssafy.roscamback.response;
+import lombok.Getter;
 
+//레코드 롬복말고 not lombok use Record
+@Getter
 public class ChatAnalysisResponse {
+
+    private String msg;
+
+
 
 }
