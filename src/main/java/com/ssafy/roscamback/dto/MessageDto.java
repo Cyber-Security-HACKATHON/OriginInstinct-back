8a62c394350f5d1d395cd0859e8c5e30006e007c
Be-HinD <kr100475@gmail.com>
2024-09-10 20:55:29 +0900

    [feat] 채팅 내역 저장 이벤트 모듈 추가

---
+++ src/main/java/com/ssafy/roscamback/dto/MessageDto.java
@@ -0,0 +1,8 @@
+package com.ssafy.roscamback.dto;
+
+public class MessageDto {
+
+    private String userId;
+    private String otherId;
+    private String data;
+}
---
+++ src/main/java/com/ssafy/roscamback/module/ChatSaveListener.java
@@ -0,0 +1,30 @@
+package com.ssafy.roscamback.module;
+
+import com.ssafy.roscamback.util.RedisUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Slf4j
+public class ChatSaveListener {
+
+    private final RedisUtil redisUtil;
+
+    public ChatSaveListener(RedisUtil redisUtil) {
+        this.redisUtil = redisUtil;
+    }
+
+    @Async
+    @EventListener
+    @Transactional
+    public void handleChatSaveEvent(ChatSaveEvent event) {
+        log.info("Chat Save Event Open");
+
+        log.info("Saving Data = {}", event.getMessage());
+        redisUtil.insert("msg", event.getMessage());
+    }
+}
